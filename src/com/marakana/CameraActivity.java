f5316acb2821541b05406c41e2ae13982e3096b5
Vincent LARK <vincent.lark@gmail.com>
2011-02-22 22:09:38 +0100

    namespace change / milestone views / search view

--- src/com/marakana/CameraActivity.java
+++ src/com/marakana/CameraActivity.java
@@ -2,9 +2,8 @@ package com.marakana;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
-import vincentlark.trac.tracdroid.R;
+import org.vl.trac.tracdroid.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
